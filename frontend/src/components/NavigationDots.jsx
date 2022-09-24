b9327facb06d83f827d10c52be1cae90b0b863ac
VanshChauhan15 <97740365+VanshChauhan15@users.noreply.github.com>
2022-09-24 14:56:30 +0530

    first commit

---
+++ frontend/src/components/NavigationDots.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavigationDots = ({active}) => {
+  return (
+    <div className='app__navigation'>
+        {
+            ['home','about','work','skills','testimonial','contacts'].map((item,index)=>( 
+            <a 
+            key={item+index}
+            href={`#${item}`}
+            className='app__navigation-dot'
+            style={active===item? {backgroundColor:'#313BAC'}:{}}
+            />
+            ))
+            }
+    </div>
+  )
+}
+
+export default NavigationDots
